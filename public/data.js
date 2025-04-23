cf9182d1a0ac94bfe87f3693e797aa4304c337d1
João Pedro Costa e Silva <joaopedrocostaesilva15@gmail.com>
2025-04-22 22:49:58 -0300

    data.js

---
+++ public/data.js
@@ -0,0 +1,88 @@
+const dadosVideos = {
+  "videos": [
+    {
+      "id": 1,
+      "titulo": "Olha que floresta bonita",
+      "descricao": "Vídeo sobre a beleza de uma floresta exuberante.",
+      "conteudo": "Neste vídeo, mostramos imagens incríveis da floresta. A experiência é relaxante e a natureza é preservada.",
+      "autor": "Autor A",
+      "destaque": true,
+      "data": "2025-03-30",
+      "categoria": "Natureza",
+      "duracao": "10:25",
+      "visualizacoes": "15.432",
+      "imagem_principal": "https://picsum.photos/id/15/800/450",
+      "imagens_complementares": [
+        { "id": 1, "src": "https://picsum.photos/id/16/800/450", "descricao": "Cena 1: Vista geral" },
+        { "id": 2, "src": "https://picsum.photos/id/17/800/450", "descricao": "Cena 2: Detalhe da flora" }
+      ]
+    },
+    {
+      "id": 2,
+      "titulo": "Festival de Música Incrível",
+      "descricao": "Cobertura do festival com atrações nacionais e internacionais.",
+      "conteudo": "O festival reuniu milhares de pessoas e contou com shows de vários artistas. Veja como foi o evento!",
+      "autor": "Autor B",
+      "destaque": true,
+      "data": "2025-03-28",
+      "categoria": "Música",
+      "duracao": "15:42",
+      "visualizacoes": "32.198",
+      "imagem_principal": "https://picsum.photos/id/30/800/450",
+      "imagens_complementares": [
+        { "id": 1, "src": "https://picsum.photos/id/39/800/450", "descricao": "Show ao vivo" },
+        { "id": 2, "src": "https://picsum.photos/id/45/800/450", "descricao": "Público animado" }
+      ]
+    },
+    {
+      "id": 3,
+      "titulo": "Vlog do Dia a Dia no Faketube",
+      "descricao": "Veja os bastidores e o dia a dia dos criadores do Faketube.",
+      "conteudo": "Neste vlog, mostramos um pouco da rotina da equipe, encontros, desafios e muitas histórias interessantes.",
+      "autor": "Autor C",
+      "destaque": false,
+      "data": "2025-03-27",
+      "categoria": "Vlogs",
+      "duracao": "8:15",
+      "visualizacoes": "7.845",
+      "imagem_principal": "https://picsum.photos/id/64/800/450",
+      "imagens_complementares": [
+        { "id": 1, "src": "https://picsum.photos/id/65/800/450", "descricao": "Encontro da equipe" }
+      ]
+    },
+    {
+      "id": 4,
+      "titulo": "Tutorial de Programação Web",
+      "descricao": "Aprenda a criar sites responsivos com HTML, CSS e JavaScript.",
+      "conteudo": "Neste tutorial completo, você vai aprender os fundamentos de desenvolvimento web e como criar sites modernos e responsivos utilizando as tecnologias mais recentes.",
+      "autor": "Autor D",
+      "destaque": true,
+      "data": "2025-03-25",
+      "categoria": "Tecnologia",
+      "duracao": "25:10",
+      "visualizacoes": "45.672",
+      "imagem_principal": "https://picsum.photos/id/4/800/450",
+      "imagens_complementares": [
+        { "id": 1, "src": "https://picsum.photos/id/48/800/450", "descricao": "Demonstração de código" },
+        { "id": 2, "src": "https://picsum.photos/id/0/800/450", "descricao": "Resultado final do projeto" }
+      ]
+    },
+    {
+      "id": 5,
+      "titulo": "Receita de Bolo de Chocolate",
+      "descricao": "Aprenda a fazer um delicioso bolo de chocolate caseiro.",
+      "conteudo": "Nesta receita, você vai aprender o passo a passo para fazer um bolo de chocolate fofinho e saboroso. Ingredientes simples e resultado incrível!",
+      "autor": "Autor E",
+      "destaque": false,
+      "data": "2025-03-22",
+      "categoria": "Culinária",
+      "duracao": "12:35",
+      "visualizacoes": "28.943",
+      "imagem_principal": "https://picsum.photos/id/102/800/450",
+      "imagens_complementares": [
+        { "id": 1, "src": "https://picsum.photos/id/292/800/450", "descricao": "Ingredientes" },
+        { "id": 2, "src": "https://picsum.photos/id/431/800/450", "descricao": "Bolo pronto" }
+      ]
+    }
+  ]
+};
